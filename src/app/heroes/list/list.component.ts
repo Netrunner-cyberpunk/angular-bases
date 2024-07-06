03d6c74ac37eeb062b69200a91a118c7b90be063
marco <mgz5489@hotmail.com>
2024-07-06 16:57:55 +0200

    primer commit

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -3,16 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-heroes-list',
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrls: ['./list.component.css']
 })
 export class ListComponent {
 
+  public heroNames: string[] = ['Spiderman','Ironman','Hulk','She Hulk', 'Thor'];
+  public deletedHero?: string;
 
-public heroNames: string[] = ['Spiderman','Ironman','Hulk','She Hulk','Thor'];
-public deletedHero?: string;
-
- removeLasthero():void {
-   this.deletedHero = this.heroNames.pop();
- }
+  removeLastHero():void {
+    this.deletedHero = this.heroNames.pop();
+  }
 
 }
